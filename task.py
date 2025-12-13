3186e3f154b7ebe226c4f90b8bc24c3a8ba86786
MatveyShulpin <76696302+MatveyShulpin@users.noreply.github.com>
2025-12-13 15:57:20 +0300

    ЛР 5. Выполненное задание

--- task.py
+++ task.py
@@ -5,6 +5,7 @@ import csv
 
 file = open('task1-en.txt', 'r') 
 text = file.read()
+file.close()
 
 pattern1 = r'[a-z-A-Z]+,'
 pattern2 = r'[+[\S]+]'
@@ -17,6 +18,7 @@ print(matches2)
 #Задание 2
 file2 = open('task2.html', 'r', encoding='utf-8')
 text2 = file2.read()
+file2.close()
 pattern3 = r'"#+\w{6}"'
 matches3 = re.findall(pattern3, text2)
 print(matches3)
@@ -57,4 +59,5 @@ for i in range(0, len(words), 5):
 with open('task3.csv', "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f, delimiter=";")
     writer.writerow(["ID", "Фамилия", "Email", "Дата регистрации", "Сайт"])
+
     writer.writerows(accounts)
